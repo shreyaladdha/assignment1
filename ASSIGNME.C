418e9825147d0ebb7c453fdc1dbb5f1058e0aeac
shreyaladdha <53753853+shreyaladdha@users.noreply.github.com>
2019-08-13 18:45:49 +0530

    Add files via upload

--- ASSIGNME.C
+++ ASSIGNME.C
@@ -7,25 +7,25 @@ void main()
 		char ID[20];
 		int age;
 		int marks;
-	}s1;
+	}s1;  //structure variable
 	clrscr();
-	printf("enter the student ID");
-	scanf("%s",s1.ID);
-	printf("enter student age");
-	scanf("%d",&s1.age);
+	printf("Enter the student ID");
+	scanf("%s",s1.ID); //accepting student id
+	printf("Enter student age");
+	scanf("%d",&s1.age); //accepting student age
 	printf("Enter marks secured");
-	scanf("%d",&s1.marks);
-	if(s1.age>=20)
+	scanf("%d",&s1.marks);//accepting student marks
+	if(s1.age>=20) //checking for criteria
 	{
 		if((s1.marks>=0)&&(s1.marks<=100))
 		{
 			if(s1.marks>=65)
-			printf("student qualifies for admission");
+			printf("student qualifies for admission");//student if admitted
 			else
 			printf("student does not qualifies for admission");
 		}
 		else
-		printf("data invalid");
+		printf("data invalid"); //if invalid data
 	}
 	else
 	printf("data invalid");
